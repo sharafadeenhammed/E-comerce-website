fbc1d1b915ff61bc8abef74b1792b696d6ec61c8
Sharafadeen Hammed <allahsegun@gmail.com>
2022-07-23 11:14:35 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+import reactDOM from "react-dom";
+
+import "./style.css"
+
+import App from "./App" 
+ 
+reactDOM.render(
+    <react.StrictMode>
+        <App/>
+    </react.StrictMode> 
+
+,document.getElementById("root"))     
